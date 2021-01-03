639b4e7e8d21ba78eafbeb73ee70dad3b042404c
RayhanHamada <hamadadev2016@gmail.com>
2021-01-04 00:16:30 +0700

    test: add test for dotenvCmdWebpack

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 import webpack, { Configuration } from 'webpack';
 import { expect } from 'chai';
 
-import { DotenvCmdWebpack } from '../src/index';
+import { dotenvCmdWebpack, DotenvCmdWebpack } from '../src/index';
 
 const config: Configuration = {
   mode: 'production',
@@ -40,6 +40,63 @@ describe('DotenvCmdWebpack class', function () {
   });
 
   const plugin = new DotenvCmdWebpack({
+    filePath: path.resolve(__dirname, '.env.json'),
+    env: 'dev',
+  });
+
+  it('should be accepted by webpack() with no error', function (done) {
+    webpack(
+      {
+        ...config,
+        plugins: [plugin],
+      },
+      function (_err, stats) {
+        expect(stats.hasErrors()).to.be.false;
+        expect(stats.compilation.errors).to.be.empty;
+        done();
+      }
+    );
+  });
+
+  it(`change "process.env.PORT" to "3000" in mockGenerated.js`, function (done) {
+    fs.promises
+      .readFile(path.resolve(__dirname, 'mockGenerated.js'), {
+        encoding: 'utf-8',
+      })
+      .then(code => {
+        expect(code).to.contain(`"3000"`);
+        done();
+      })
+      .catch(() => {
+        expect.fail('failed to open mockGenerated.js');
+      });
+  });
+});
+
+describe('dotenvCmdWebpack', function () {
+  this.beforeAll(function (done) {
+    fs.promises
+      .unlink(path.resolve(__dirname, 'mockGenerated.js'))
+      .then(() => {
+        console.log(`mockGenerated.js deleted !`);
+        done();
+      })
+      .catch(() => {
+        console.log(`failed to delete mockGenerated.js`);
+        done();
+      });
+  });
+
+  it('should read file with no error', function () {
+    expect(
+      dotenvCmdWebpack({
+        filePath: path.resolve(__dirname, '.env.json'),
+        env: 'dev',
+      })
+    ).to.not.throw;
+  });
+
+  const plugin = dotenvCmdWebpack({
     filePath: path.resolve(__dirname, '.env.json'),
     env: 'dev',
   });
