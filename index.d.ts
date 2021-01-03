dfb9c5b329cf9b48cd032f36e056f7959eb0dda7
RayhanHamada <hamadadev2016@gmail.com>
2021-01-03 16:20:33 +0700

    fix: ignore dist and mockGenerated.js from git and change some config in tsconfig and package.json

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Compiler, DefinePlugin } from 'webpack';
-import { WebpackEnvConfig } from './types';
-export declare class DotenvCmdWebpack extends DefinePlugin {
-    private config;
-    constructor(config: WebpackEnvConfig);
-    apply(compiler: Compiler): void;
-}
--- utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.debug = void 0;
-function debug(msg, debug) {
-    if (debug) {
-        console.log(`dotenv-cmd-webpack   : ${msg}`);
-    }
-}
-exports.debug = debug;
